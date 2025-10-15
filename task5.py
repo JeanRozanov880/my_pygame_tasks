f24b44eebcf4a2ba56b7fc28c57c2aefddd5cfde
JeanRozanov880 <jeanrozanov@gmail.com>
2025-10-15 16:07:52 +0400

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,5 +1,3 @@
-# Часть 1:
-
 import pygame as pg
 import random
 
@@ -15,7 +13,10 @@ clock = pg.time.Clock()
 
 # до начала игрового цикла отображаем объекты:
 # координаты центра круга
-r = 30
+r = 35
+inc = 1 # насколько будем увеличивать
+min_r = 10
+max_r = 120
 x, y = random.randint(0 + r, WIDTH - r), random.randint(0 + r, HEIGHT - r)  # координаты круга
 pg.draw.circle(screen, ORANGE, (x, y), r)  # рисуем круг
 pg.display.update()  # обновляем окно
@@ -30,6 +31,13 @@ while flag_play:
             flag_play = False
         if event.type == pg.MOUSEMOTION:
             mouse_pos = event.pos
+        if event.type == pg.MOUSEWHEEL: # изменяем размеры шара с помощью колесика
+            if event.y > 0: # увеличиваем шар
+                if r < max_r:
+                    r += inc
+            if event.y < 0: # уменьшаем шар
+                if r > min_r:
+                    r -= inc
 
             break
     if not flag_play:
@@ -41,12 +49,17 @@ while flag_play:
     dist = ((x - mouse_pos[0]) ** 2 + (y - mouse_pos[1]) ** 2) ** 0.5
 
     pressed = pg.mouse.get_pressed()
-    if dist <= r:
-        if pressed[1]:
+    if dist <= r: # проверяем, наведена ли мышь на шар
+        if pressed[1]: # при нажатии на колесико шар лопается и отрисовывается новый
             x, y = random.randint(0 + r, WIDTH - r), random.randint(0 + r, HEIGHT - r)
-            screen.fill(MINT)
-            pg.draw.circle(screen, ORANGE, (x, y), r)  # рисуем новый, сдвинутый круг
-            pg.display.update()
+
+        if pressed[0]: # увеличение шара на м1
+            if r < max_r:
+                r += inc
+                
+        if pressed[2]: # уменьшение шара на м2
+            if r > min_r:
+                r -= inc
 
     screen.fill(MINT)  # заливаем фон, стирая предыдущий круг
     pg.draw.circle(screen, ORANGE, (x, y), r)  # рисуем новый, сдвинутый круг

124d65c8f4d0e0eb5b80b9c02535022b24e1b842
JeanRozanov880 <jeanrozanov@gmail.com>
2025-10-22 09:48:05 +0400

    Update and rename task7.py to task6.py

--- task6.py
+++ task6.py
@@ -23,15 +23,14 @@ pg.draw.rect(background_sf, DAY_BLUE, (0, WIN_HEIGHT * 3 / 14, WIN_WIDTH, WIN_HE
 pg.draw.rect(background_sf, DAY_BLUE, (0, WIN_HEIGHT * 3 / 5, WIN_WIDTH, WIN_HEIGHT * 1 / 5))
 
 # слой №2 - первый шар
-ball1_sf = pg.Surface((WIN_HEIGHT * 1 / 4, WIN_HEIGHT * 1 / 4), pg.SRCALPHA)
-ball1_rect = ball1_sf.get_rect(centerx=WIN_WIDTH / 2)
-ball1_rect.top = WIN_HEIGHT * 3 / 4
+ball1_sf = pg.Surface((BALL_WIDTH, BALL_HEIGHT), pg.SRCALPHA)
+ball1_rect = ball1_sf.get_rect(centerx=WIN_WIDTH / 20)
 ball1_sf.fill((0, 0, 0, 0))  # сначала делаем всю поверхность полностью прозрачной
 pg.draw.circle(ball1_sf, (*RED, 70), (BALL_WIDTH / 2, BALL_HEIGHT / 2), r1)
 
 # слой №3 - второй шар
 ball2_sf = pg.Surface((BALL_WIDTH, BALL_HEIGHT), pg.SRCALPHA)
-ball2_rect = ball2_sf.get_rect(centerx=WIN_WIDTH / 2)
+ball2_rect = ball2_sf.get_rect(centerx=WIN_WIDTH / 20)
 ball2_sf.fill((0, 0, 0, 0))  # сначала делаем всю поверхность полностью прозрачной
 pg.draw.circle(ball2_sf, (*GREEN, 255), (BALL_WIDTH / 2, BALL_HEIGHT / 2), r2)
 
@@ -61,6 +60,23 @@ while flag_play:
         break
 
     # изменение характеристик объектов:
+    # движение 1-ого шара:
+    if ball1_rect.right >= WIN_WIDTH + r1: # если шар дошел до правой границы
+        direction1 = -1 # меняем направление налево
+        ball1_rect.bottom = BALL_HEIGHT * 1.61 # переносим вниз
+    elif ball1_rect.left <= 0 - r1: # если шар дошел до левой границы
+        direction1 = 1 # меняем направление направо
+        ball1_rect.top = 0 # переносим вверх (исходное положение)
+    ball1_rect.right += speed1 * direction1 # обработка самого движения
+        
+    # движение 2-ого шара (аналогично первому):
+    if ball2_rect.right >= WIN_WIDTH + r2:
+        direction2 = -1
+        ball2_rect.bottom = BALL_HEIGHT * 1.61
+    elif ball2_rect.left <= 0 - r2:
+        direction2 = 1
+        ball2_rect.top = 0
+    ball2_rect.right += speed2 * direction2
 
     screen.blit(background_sf, (0, 0))
     screen.blit(ball1_sf, ball1_rect)
